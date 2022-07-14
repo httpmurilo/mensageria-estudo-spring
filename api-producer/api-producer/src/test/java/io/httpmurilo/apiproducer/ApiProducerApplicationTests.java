3977ab86b437b8a810a097b5ecb4357a6e80e1e5
httpmurilo <muriloeduardo1997@outlook.com>
2022-07-14 20:11:57 -0300

    first commit

---
+++ api-producer/api-producer/src/test/java/io/httpmurilo/apiproducer/ApiProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.httpmurilo.apiproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
